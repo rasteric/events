3a684bb1bd5ccf87cf3b67ba84b3ff3f81eec810
E. H. Rast <erich@snafu.de>
2022-05-14 20:56:10 +0100

    fix: made events with struct receiver, no need to use pointers here

--- events.go
+++ events.go
@@ -8,24 +8,24 @@ type Event struct {
 }
 
 // New creates a new event with given ID and optional arguments.
-func New(id int, args ...any) *Event {
-	return &Event{ID: id, Args: args}
+func New(id int, args ...any) Event {
+	return Event{ID: id, Args: args}
 }
 
 // Arg returns the nth argument and true, nil and false if no argument with that index exists.
-func (e *Event) Arg(idx int) (any, ok) {
-	if e == nil || e.Args == nil {
+func (e Event) Arg(idx int) (any, bool) {
+	if e.Args == nil {
 		return nil, false
 	}
 	if idx < 0 || idx > len(e.Args)-1 {
 		return nil, false
 	}
-	return e.Args(idx)
+	return e.Args[idx], true
 }
 
 // Count returns the event's number of arguments.
-func (e *Event) Count() int {
-	if e == nil || e.Args == nil {
+func (e Event) Count() int {
+	if e.Args == nil {
 		return 0
 	}
 	return len(e.Args)
