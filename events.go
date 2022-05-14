0c1c06823b9a21c281c8b89bdea5b16e85f45c1a
E. H. Rast <erich@snafu.de>
2022-05-14 20:50:20 +0100

    feat: basic event structure with arbitrary arguments

--- events.go
+++ events.go
@@ -1,16 +1,32 @@
 package events
 
-
 // Event describes an event with an ID and an arbitrary datum associated with the ID.
-// The subscriber is responsible to correctly type dispatch on the Datum, based on the event type.
+// The subscriber is responsible to correctly type dispatch on the Datum, based on the event ID.
 type Event struct {
 	ID   int
-	Arg1 any
-	Arg2 any
-	Arg3 any
-	Arg4 any
-	Arg5 any
-	Arg6 any
-	Err  error
+	Args []any
 }
 
+// New creates a new event with given ID and optional arguments.
+func New(id int, args ...any) *Event {
+	return &Event{ID: id, Args: args}
+}
+
+// Arg returns the nth argument and true, nil and false if no argument with that index exists.
+func (e *Event) Arg(idx int) (any, ok) {
+	if e == nil || e.Args == nil {
+		return nil, false
+	}
+	if idx < 0 || idx > len(e.Args)-1 {
+		return nil, false
+	}
+	return e.Args(idx)
+}
+
+// Count returns the event's number of arguments.
+func (e *Event) Count() int {
+	if e == nil || e.Args == nil {
+		return 0
+	}
+	return len(e.Args)
+}
