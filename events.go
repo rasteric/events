5cada6dc6976ac1e2c2c558e04605083ee57f849
E. H. Rast <erich@snafu.de>
2022-05-14 21:19:14 +0100

    Moved pubsub to Events management

--- events.go
+++ events.go
@@ -1,5 +1,21 @@
 package events
 
+import (
+	"time"
+
+	"github.com/mattn/go-pubsub"
+)
+
+var ps *pubsub.PubSub
+var lastTime time.Time
+var lastEvent Event
+
+func init() {
+	ps = pubsub.New()
+	lastTime = time.Now()
+	lastEvent = Event{}
+}
+
 // Event describes an event with an ID and an arbitrary datum associated with the ID.
 // The subscriber is responsible to correctly type dispatch on the Datum, based on the event ID.
 type Event struct {
@@ -29,4 +45,41 @@ func (e Event) Count() int {
 		return 0
 	}
 	return len(e.Args)
+}
+
+// PubEvent publishes an event.
+func PubEvent(evt Event) {
+	ps.Pub(evt)
+}
+
+// Pub publishes an event given by the event ID and optional arguments.
+func Pub(id int, args ...any) {
+	PubEvent(New(id, args...))
+}
+
+// PubLimitedEvent publishes an event but drops the same event was published no longer than 200 milliseconds
+// ago. This is used for unimportant, ephemeral events to rate limit them.
+func PubLimitedEvent(evt Event) {
+	now := time.Now()
+	if evt.ID == lastEvent.ID && now.Sub(lastTime) < 200*time.Millisecond {
+		return
+	}
+	lastTime = now
+	lastEvent = evt
+	PubEvent(evt)
+}
+
+// PubLimited publishes an event given by the ID and optional arguments.
+func PubLimited(id int, args ...any) {
+	PubLimitedEvent(New(id, args...))
+}
+
+// SubEvent subscribes to an event.
+func SubEvent(fn func(evt Event)) {
+	ps.Sub(fn)
+}
+
+// Sub subscribes to an event with a function that receives an event id and optional many arguments.
+func Sub(fn func(id int, args ...any)) {
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rasteric/events
 
 go 1.18
+
+require github.com/mattn/go-pubsub v0.0.0-20160821075316-7a151c7747cd // indirect
