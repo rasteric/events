39a456cc98eb7d9c963c9698f3ea4a76e20ccd14
E. H. Rast <erich@snafu.de>
2022-05-16 17:09:45 +0100

    feat: added basic Readme.md

--- events.go
+++ events.go
@@ -1,17 +1,20 @@
 package events
 
 import (
+	"sync"
 	"time"
-
-	"github.com/mattn/go-pubsub"
 )
 
-var ps *pubsub.PubSub
+// TODO implement pubsub here myself
 var lastTime time.Time
 var lastEvent Event
+var subscriptions []func(evt Event)
+var mutex sync.RWMutex
+var callbacks map[int][]func(evt Event)
 
 func init() {
-	ps = pubsub.New()
+	subscriptions = make([]func(evt Event), 0)
+	callbacks = make(map[int][]func(evt Event))
 	lastTime = time.Now()
 	lastEvent = Event{}
 }
@@ -49,7 +52,17 @@ func (e Event) Count() int {
 
 // PubEvent publishes an event.
 func PubEvent(evt Event) {
-	ps.Pub(evt)
+	mutex.RLock()
+	defer mutex.RUnlock()
+	cbs, ok := callbacks[evt.ID]
+	if ok {
+		for _, cb := range cbs {
+			cb(evt)
+		}
+	}
+	for _, fn := range subscriptions {
+		fn(evt)
+	}
 }
 
 // Pub publishes an event given by the event ID and optional arguments.
@@ -74,12 +87,25 @@ func PubLimited(id int, args ...any) {
 	PubLimitedEvent(New(id, args...))
 }
 
-// SubEvent subscribes to an event.
-func SubEvent(fn func(evt Event)) {
-	ps.Sub(fn)
+// Sub subscribes to an event.
+func Sub(fn func(evt Event)) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	subscriptions = append(subscriptions, fn)
 }
 
-// Sub subscribes to an event with a function that receives an event id and optional many arguments.
-func Sub(fn func(id int, args ...any)) {
-
+// SubCb subscribes a callback function to an event ID. If an event with the given ID type
+// occurs, the callback function is executed with the event as argument. This can be used for
+// certain cases when Sub alone would lead to race conditions.
+func SubCb(eventID int, fn func(evt Event)) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	cbs, ok := callbacks[eventID]
+	if ok {
+		cbs = append(cbs, fn)
+		return
+	}
+	queue := make([]func(evt Event), 1)
+	queue[0] = fn
+	callbacks[eventID] = queue
 }
